d1a00fc8adbe20c502f790c91fb1720a0d3c3de1
Salamander <mokmaard646@gmail.com>
2023-02-25 20:44:47 +0700

    first_commit

---
+++ next-login/pages/api/[..nextauth].js
@@ -0,0 +1,22 @@
+import NextAuth from 'next-auth';
+
+export default NextAuth( {
+    providers: [
+        CredentialProvider({
+            name: "Credentials",
+            credentials: {
+                username: { label: "Username", type: "text", placeholder: "jsmith"},
+                password: { label: "Password", type: "password"}    
+            },
+            async authorize(credentials, req) {
+                const user = {id: "1", name: "J Smith", email: "jsmith@example.com"}
+
+                if (user) {
+                    return user;
+                } else {
+                    return null;
+                }
+            }
+        })
+    ]
+})
